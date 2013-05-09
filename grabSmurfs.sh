2b41a635d0ffb00a0c56465a8ea6649d8e3a54b3
Adrian Bjugård <adrian@qng.se>
2013-05-09 23:35:31 +0200

    data-collector: improved code

--- grabSmurfs.sh
+++ grabSmurfs.sh
@@ -9,10 +9,28 @@
 # 	aircrack-ng
 # Arguments:
 # 	1. How long to run the script for
+#	2. Address to POST to
 
 # -- Change this to your interface --
 IFACE="wlan0"
 # -----------------------------------
+
+# Exit if no site argument given
+if [ -z "$2" ]; then
+	echo "Usage: grabSmurfs.sh <time to scan> <address to POST to>"
+	exit
+fi
+
+# Define utilities
+AIRMON="/usr/sbin/airmon-ng"
+AIRDUMP="/usr/sbin/airodump-ng"
+
+SLEEP="/usr/bin/sleep"
+KILLALL="/usr/bin/killall"
+SED="/usr/bin/sed"
+CUT="/usr/bin/cut"
+RM="/usr/bin/rm"
+WGET="/usr/bin/wget"
 
 # Set variables
 if [[ "$1" =~ ^[0-9]+$ ]]; then
@@ -26,14 +44,31 @@ MON="mon0"
 
 # Make sure interface is available
 if [ "$(ifconfig | grep -o $MON)" != "$MON" ]; then
-	/usr/sbin/airmon-ng start $IFACE
+	$AIRMON start $IFACE
 fi
 
 # Generate data
-(/bin/sleep $RUNTIME && /usr/bin/killall airodump-ng) &
-/usr/sbin/airodump-ng --output-format csv mon0 -w out
+($SLEEP $RUNTIME && $KILLALL airodump-ng) &
+$AIRDUMP --output-format csv mon0 -w out
 
 # Clean output
-/bin/sed -i "1,/$STRING/d" out-01.csv
-/usr/bin/cut -f1 -d"," out-01.csv > out.csv
-/bin/rm out-01.csv
+$SED -i "1,/$STRING/d" out-01.csv
+$CUT -f1 -d"," out-01.csv > out.txt
+$SED -i -e '$d' out.txt
+
+# Format JSON
+JSON="$(echo -e "{\"mac\":[")"
+while read -r line; do
+	JSON+="$(echo -e "\"$line\",")"
+done < out.txt
+JSON+="$(echo -e "\b]}")"
+
+# Prints out JSON
+# echo -e $JSON > out.json
+
+# Sends JSON via wget
+# echo $WGET "$2?\"site\"=\"addMacs\"\&\"json\"=$JSON"
+
+# Clean up
+$RM out-01.csv
+$RM out.txt
