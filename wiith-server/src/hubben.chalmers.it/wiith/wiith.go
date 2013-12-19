daa67888dc67fc0b4213f928642139b28f57a627
Emil Edholm <emil@edholm.it>
2013-12-19 22:58:20 +0100

    Added license and updated readme

--- wiith-server/src/hubben.chalmers.it/wiith/wiith.go
+++ wiith-server/src/hubben.chalmers.it/wiith/wiith.go
@@ -1,3 +1,22 @@
+/* This is the WhoIsInTheHubb server application written i Go.
+ * The server records nearby WiFi clients (using their MAC-addresses)
+ * and stores statistics about them in a database.
+
+ * Copyright (C) 2013 Emil 'Eda' Edholm (digIT13)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package main
 
 import (
