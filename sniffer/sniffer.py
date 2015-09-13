9bd0c1cd9cab7dfed0853426571e9a5f31a68045
Emil Edholm <bobby@verypublic.org>
2015-09-13 15:17:37 +0200

    Basic printing of src address

--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -2,13 +2,20 @@
 #!/usr/bin/python3
 
 import reqs
+import blacklist
+import pyshark
 
 def main():
-    req = reqs.Requirements()
-    if not req.check():
-        print('Fix the above before continuing')
-        exit(1)
-
+    blist = blacklist.Blacklist()
+    #req = reqs.Requirements()
+    #if not req.check():
+    #    print('Fix the above before continuing')
+    #    exit(1)
+    cap = pyshark.LiveCapture(interface="hubbit", only_summaries=False, bpf_filter=blist.create_filter(blacklist.Filter.Capture))
+    for frame in cap.sniff_continuously():
+        wlan = frame['wlan']
+        if hasattr(wlan, "sa"):
+            print(wlan.sa)
 
 if __name__ == '__main__':
     main()
