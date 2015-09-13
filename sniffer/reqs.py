a11c16d7d819801d0c67258bcbf9f7245405554e
Emil Edholm <bobby@verypublic.org>
2015-09-13 19:24:38 +0200

    Sniffing done

--- sniffer/reqs.py
+++ sniffer/reqs.py
@@ -1,34 +1,20 @@
-from shutil import which
-from subprocess import Popen
-
 class Requirements():
     """This class manages and checks requirements before running the sniffer"""
 
-    def __init__(self, iface='mon0'):
+    def __init__(self, iface='hubbit'):
         self.iface = iface
 
     def check(self):
         have_reqs = True
-        if not which('ip'):
-            print('Unable to find ip program. Unable to change to promiscious mode')
-            have_reqs = False
-        if not which('iw'):
-            print('Unable to find iw. Needed for checking wireless interface')
-            have_reqs = False
-
-        if not self._mon_mode():
-            print("Monitor mode not enabled")
+        if not self._iface_exists():
+            print("Network interface \'" + self.iface + "\' doesn't exist. Run scripts/setup-mon-iface.sh as root")
             have_reqs = False
         return have_reqs
 
-    def _mon_mode(self):
-        print("Hello")
-        with Popen(["iw", "dev", self.iface, "info"], shell=True) as iw:
-            print(iw.stdout.read())
-        return False
+    def _iface_exists(self):
+        content = None
+        with open("/proc/net/dev") as f:
+            content = f.read()
 
-    def _promisc(self):
-        return False
-
-
+        return self.iface in content
 
--- sniffer/sniffer.py
+++ sniffer/sniffer.py
@@ -3,19 +3,97 @@
 
 import reqs
 import blacklist
-import pyshark
+import subprocess
+import threading
+import time
+import signal
+import re
+
+class MacStorage():
+    def __init__(self):
+        self._lock = threading.Lock()
+        self._macs = dict()
+
+    def seen(self, mac):
+        ''' Add mac to dict, where time is the time the mac was last seen '''
+        self._lock.acquire()
+        self._macs[mac] = self._macs.get(mac, 0) + 1
+        self._lock.release()
+
+    def clear(self):
+        self._lock.acquire()
+        self._macs.clear()
+        self._lock.release()
+
+    def list_and_clear(self):
+        self._lock.acquire()
+        maclist = [(k,v) for k,v in self._macs.items()]
+        self._macs.clear()
+        self._lock.release()
+
+        return maclist
+
+class Capture(threading.Thread):
+    def __init__(self, storage):
+        self._storage = storage
+        threading.Thread.__init__(self)
+
+    def _build_command(self):
+        filter = blacklist.Blacklist().create_filter()
+        return ['tshark', '-i' 'hubbit', '-p', '-l', '-n', '-T', 'fields', '-e', 'wlan.sa', filter]
+
+    def run(self):
+        self._tshark_proc = subprocess.Popen(self._build_command(), stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+        lines_it = iter(self._tshark_proc.stdout.readline, b'')
+        for sa in lines_it:
+            if len(sa) > 1:
+                self._storage.seen(sa.decode('utf-8').strip())
+
+    def stop(self):
+        self._tshark_proc.terminate() # Send sigterm to subprocess
+
+
+class Main:
+    def __init__(self):
+        self._storage = MacStorage()
+        self._keep_capturing = True
+        self._sigint = False
+
+    def handle_sigusr1(self, signal, frame):
+        print("Caught SIGUSR1, reloading blacklist")
+
+        if self._cap is not None:
+            self._cap.stop()
+            self._keep_capturing = False
+
+    def handle_sigint(self, signal, frame):
+        print("Caught SIGINT")
+        self._cap.stop()
+        self._keep_capturing = False
+        self._sigint = True
+
+
+    def run(self):
+        signal.signal(signal.SIGUSR1, self.handle_sigusr1)
+        signal.signal(signal.SIGINT, self.handle_sigint)
+
+        while not self._sigint:
+            self._keep_capturing = True
+            self._cap = Capture(self._storage)
+            self._cap.start()
+            while self._keep_capturing:
+                time.sleep(5)
+                print(self._storage.list_and_clear())
+            self._storage.clear()
+            self._cap.join()
 
 def main():
-    blist = blacklist.Blacklist()
-    #req = reqs.Requirements()
-    #if not req.check():
-    #    print('Fix the above before continuing')
-    #    exit(1)
-    cap = pyshark.LiveCapture(interface="hubbit", only_summaries=False, bpf_filter=blist.create_filter(blacklist.Filter.Capture))
-    for frame in cap.sniff_continuously():
-        wlan = frame['wlan']
-        if hasattr(wlan, "sa"):
-            print(wlan.sa)
+    req = reqs.Requirements()
+    if not req.check():
+        print('Fix the above before continuing')
+        exit(1)
+    m = Main()
+    m.run()
 
 if __name__ == '__main__':
     main()
